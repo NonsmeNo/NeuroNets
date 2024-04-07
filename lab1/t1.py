c0e168f66d77fedaf4330600c40722e853668476
NonsmeNo <Lera.53rus@yandex.ru>
2024-04-07 11:12:18 +0300

    lab1

---
+++ lab1/t1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#_*_ coding: utf-8 _*_
+
+from random import random
+from math import sqrt
+class Neuron:
+    def __init__(self):
+        self.nu = 0.5 #коэффициент обучения
+        self.w = [random(), random()] #веса
+        divider = sqrt(self.w[0] ** 2 + self.w[1] ** 2) #нормирование весов
+        self.w[0] /= divider
+        self.w[1] /= divider
+
+    def calculate(self, x):
+        return self.w[0] * x[0] + self.w[1] * x[1] #взвешенная сумма
+
+    def recalculate(self, x): #правило Гроссберга
+        self.w[0] += self.nu * (x[0] - self.w[0])
+        self.w[1] += self.nu * (x[1] - self.w[1])
+
+class NeuralNetwork:
+    def __init__(self):
+        self.x = [ #входные обучающие векторы
+            [0.97, 0.2],
+            [1, 0],
+            [-0.72, 0.7],
+            [-0.67, 0.74],
+            [-0.8, 0.6],
+            [0, -1],
+            [0.2, -0.97],
+            [-0.3, -0.95]
+        ]
+
+        self.neurons = [Neuron() for i in range(4)] #определение начальных весов
+
+    def __str__(self):
+        s = ""
+        for i, neuron in enumerate(self.neurons):
+            s += f'w{str(i+1)}: {neuron.w}\n'
+        return s
+
+    def start(self):
+        u = [0 for i in range(4)] #вектор выходных сигналов
+        number_wins = [0 for i in range(4)]
+        for i in range(len(self.x)):
+            for j in range(4):
+                    u[j] = self.neurons[j].calculate(self.x[i])
+            j = u.index(max(u))
+            number_wins[j] += 1
+            print('x = ' + str(self.x[i]))
+            print('выиграл ' + str(j + 1) + ' нейрон')
+            self.neurons[j].recalculate(self.x[i]) #победитель меняет веса
+            print(f'измененные веса победителя: {str(self.neurons[j].w)} \n')
+
+nn = NeuralNetwork()
+print('\n')
+print('начальные веса' + '\n' + str(nn))
+nn.start()
+print('конечные веса' + '\n' + str(nn))
