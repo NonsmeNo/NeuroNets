31198697ad769119293ec92f20f8cfefc9d35587
Lera Panicheva <Lera.53rus@yandex.ru>
2024-04-28 18:41:44 +0300

    .

--- lab3/t1.py
+++ lab3/t1.py
@@ -1,4 +1,4 @@
-from math import exp, fabs
+from math import *
 import numpy as np
 from numpy import linalg as la
 import matplotlib.pyplot as plt
@@ -6,15 +6,14 @@ import matplotlib.pyplot as plt
 class Neuron:
     center = 0
 
-    def __init__(self, center):
-        # инициализируем нейрон центром
+    def __init__(self, center): # центр нейрона
         self.center = center
 
-    def count(self, x):
-        return exp(-((x - self.center) ** 2) / ((2 * 1.5 ** 2)))
+    def calculate(self, x): # функция Гаусса
+        return exp(-((x - self.center) ** 2) / (2 * 1.5 ** 2))
 
-# обучающая выборка
-values = [
+# обучающие примеры
+inputs = [
     [-2.0, -0.48],
     [-1.5, -0.78],
     [-1.0, -0.83],
@@ -26,53 +25,46 @@ values = [
     [2.0, 0.20]
 ]
 
-x = [value[0] for value in values]
-y = np.array([[values[i][1]] for i in range(9)])
+x = [i[0] for i in inputs]
+y = np.array([[inputs[i][1]] for i in range(len(inputs))])
+# исходные точки на графике
+plt.scatter(x, y, c="orange")
 
-# центры (пока не понятно че это такое)
+# инициализация центров скрытых нейронов в опытах 1, 3, 5, 7 и 9
 centers = [-2.0, -1.0, 0.0, 1.0, 2.0]
-# радиус тоже не понятно зачем
-radius = 1.5
-# тогда параметр a будет равен 0.22
 
 # создаем нейроны
 neurons = [Neuron(centers[i]) for i in range(len(centers))]
 
-# после создания нейронов нужно создать характеристическую матрицу
-# количество строк этой матрицы = количеству примеров из выборки
-# количество столбцов - количество нейронов
-h = np.array([[neurons[i].count(x[j]) for i in range(len(centers))] for j in range(len(x))])
+# характеристическая матрица
+G = [[neurons[i].calculate(x[j]) for i in range(len(centers))] for j in range(len(x))]
+G = np.array(G)
 print("Характеристическая матрица:")
-print(h)
+print(G)
 
-# теперь определяем синаптические коэффициенты выходного нейрона
-# для этого используем еще одну формулу
-# w = (h^T * h)^(-1) * h^T * y
-
-# коротко о том, что обозначает каждая функция
-# dot - скалярное произведение массивов
-# inv - обратная матрица
-# transpose - транспонированная
-w = np.dot(la.inv(np.dot(h.transpose(), h)), np.dot(h.transpose(), y))
-print("Весовые коэффициенты:")
+# веса по формуле:
+# w = (G^T * G)^(-1) * G^T * y
+w = np.dot(la.inv(np.dot(G.transpose(), G)), np.dot(G.transpose(), y))
+print("Веса")
 print(w)
 
-# теперь считаем выходные значения на основе весовых коэффициентов
-answers = [sum([neurons[i].count(value[0]) * w[i][0] for i in range(5)]) for value in values]
+# выходные значения на основе весовых коэффициентов
+outputs = [sum([neurons[i].calculate(val[0]) * w[i][0] for i in range(5)]) for val in inputs]
+
+outputs = np.array(outputs)
+print(outputs)
 
 # считаем среднюю относительную ошибку аппроксимации
-n = len(x)
-s = 0
+n = len(y)
+summ= 0
 for i in range(n):
-    s += fabs(1 - (y[i][0] / answers[i]))
-print("Средняя относительная ошибка аппроксимации " + str(s / n * 100) + " %")
+    summ += fabs(1 - (y[i][0] / outputs[i]))
+print("Средняя ошибка аппроксимации " + str(round((summ / n ), 5)))
 
-
-# теперь нужно все нарисовать
-plt.scatter(x, y, c="slateblue", label="Исходные точки выборки")
-plt.plot(x, answers, color="darkturquoise", label="Полученная аппроксимация")
-plt.legend()
-plt.grid(True)
-plt.title("Положение исходных точек\n  и график аппроксимации")
+#график
+plt.scatter(x, outputs)
+plt.plot(x, outputs)
+plt.grid(color='r', linestyle='--')
+plt.title("Исходные точки, получившиеся и график аппроксимации")
 plt.show()
 
